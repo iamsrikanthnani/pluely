0d24399bc34de848b7b6f84c3e044dfa31a41454
TanteJossa <joostkkoch@gmail.com>
2025-09-07 00:13:03 +0200

    Add math and raw HTML support to Markdown component
    Added remark-math and rehype-katex to enable LaTeX math rendering in Markdown, and rehype-raw to allow raw HTML. Updated dependencies in package.json and package-lock.json accordingly.

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,10 +1,15 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeRaw from 'rehype-raw'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+import 'katex/dist/katex.min.css'
 
 export const Markdown = ({ children }: { children: string }) => {
   return (
     <ReactMarkdown
-      remarkPlugins={[remarkGfm]}
+      remarkPlugins={[remarkGfm, remarkMath]}
+      rehypePlugins={[ rehypeRaw, rehypeKatex ]}
       components={{
         pre: ({ node, ...props }) => (
           <pre {...props} className="whitespace-pre-wrap break-words" />
